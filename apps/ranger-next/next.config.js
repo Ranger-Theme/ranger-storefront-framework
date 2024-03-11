90d830b1e9a888feb5b84f81a23544bf7090f8dd
caesar <caesar.luo@silksoftware.com>
2024-03-11 16:00:45 +0800

    chore: linter

--- apps/ranger-next/next.config.js
+++ apps/ranger-next/next.config.js
@@ -1,11 +1,11 @@
-const nextConfig = require("@ranger/nextjs-config");
+const nextConfig = require('@ranger/nextjs-config')
 
-const pkg = require("./package.json");
+const pkg = require('./package.json')
 
 /** @type {import('next').NextConfig} */
 module.exports = nextConfig({
   pkg,
   dirname: process.cwd(),
   timestamp: new Date().getTime(),
-  transpilePackages: ["@ranger/ui-theme"],
-});
+  transpilePackages: ['@ranger/ui-theme']
+})
