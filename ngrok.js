9771af8fa90dc8e7680c3c0c1f3515e1ecb607ce
caesar <caesar.luo@silksoftware.com>
2024-04-09 13:33:29 +0800

    feat: elfsight widget

--- ngrok.js
+++ ngrok.js
@@ -2,12 +2,27 @@ const ngrok = require('ngrok')
 const cp = require('child_process')
 
 const bootstrap = async () => {
+  const port = 3000
   const url = await ngrok.connect({
     proto: 'http', // http|tcp|tls, defaults to http
-    addr: 3000, // port or network address, defaults to 80
+    addr: port, // port or network address, defaults to 80
     authtoken: '2ZqIexMonOQDvGAs3e1YgNiKggG_ZYCvseao9nJhWfgus76k' // your authtoken from ngrok.com
   })
+
   console.info(`🚀 Server ready at: ${url}`)
+
+  switch (process.platform) {
+    // macos
+    case 'darwin':
+      cp.exec(`open ${url}`)
+      break
+    // windows
+    case 'win32':
+      cp.exec(`start ${url}`)
+      break
+    default:
+      cp.exec(`open ${url}`)
+  }
 }
 
 bootstrap()
