bee8af2f69eae98605b6979553563b274aa69b64
caesar <454451758@qq.com>
2024-07-05 11:01:48 +0800

    chore: ranger

--- packages/ranger-commitlint-config/bin.js
+++ packages/ranger-commitlint-config/bin.js
@@ -1,20 +1,13 @@
 #!/usr/bin/env node
 const spawn = require('cross-spawn')
 const path = require('path')
-const fs = require('fs')
 
 // Path to the @commitlint/cli binary
 const binPath = path.resolve(__dirname, 'node_modules', '.bin', 'commitlint')
 
-// Create a temporary commit message
-const commitMsgFilePath = path.resolve(process.cwd(), '.git', 'COMMIT_EDITMSG')
-const exsit = fs.existsSync(commitMsgFilePath)
-const message = 'chore: correct minor typos in code'
-
 // Command and arguments
 const command = process.platform === 'win32' ? 'cmd' : binPath
-const format = exsit ? ['--edit'] : ['--edit', message]
-const args = process.platform === 'win32' ? ['/c', binPath, ...format] : ['--edit']
+const args = process.platform === 'win32' ? ['/c', binPath, '--edit'] : ['--edit']
 
 // Execute the command
 const result = spawn.sync(command, args, { stdio: 'inherit', encoding: 'utf-8', shell: true })
