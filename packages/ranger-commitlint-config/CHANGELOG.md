ff51f0c49f2dc2797bfa47dfeb7c8916cb46d0b7
caesar <454451758@qq.com>
2024-07-06 20:56:41 +0800

    feat: code

--- packages/ranger-commitlint-config/CHANGELOG.md
+++ packages/ranger-commitlint-config/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ranger-theme/commitlint-config
 
+## 0.3.0
+
+### Minor Changes
+
+- solve the spawn
+
 ## 0.2.0
 
 ### Minor Changes
--- packages/ranger-commitlint-config/bin.js
+++ packages/ranger-commitlint-config/bin.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 const spawn = require('cross-spawn')
+const fs = require('fs')
 const path = require('path')
 
 // Path to the @commitlint/cli
 const binPath = path.resolve(__dirname)
 
+// Package name
+const excutePath = path.resolve(process.cwd())
+
+// check pnpm-lock.yaml exist
+const isUsingPnpm = fs.existsSync(path.join(excutePath, 'pnpm-lock.yaml'))
+
+// check yarn.lock exist
+const isUsingYarn = fs.existsSync(path.join(excutePath, 'yarn.lock'))
+
 // Command and arguments
-const command = 'pnpm'
+const origin = isUsingYarn ? 'yarn' : 'npm'
+const command = isUsingPnpm ? 'pnpm' : origin
 const args = ['commitlint', '--edit']
 
 // Execute the command
