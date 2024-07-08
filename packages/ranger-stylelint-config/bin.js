6a262445c5b537f882b10221a2bd42907affdb35
caesar <454451758@qq.com>
2024-07-08 14:45:24 +0800

    feat: sample product

--- packages/ranger-stylelint-config/bin.js
+++ packages/ranger-stylelint-config/bin.js
@@ -26,22 +26,12 @@ const result = spawn(command, args, {
   encoding: 'utf-8',
   stdio: 'inherit',
   shell: true,
-  timeout: 2000
+  timeout: 5000
 })
 
 if (result.error) {
-  // result.kill('SIGKILL')
   console.error(`Error executing command: ${result.error.message}`)
   process.exit(1)
 }
 
-// process.exit(result.status)
-// child.on('error', (error) => {
-//   console.error(`Error: ${error.message}`)
-//   clearTimeout(timeout)
-// })
-
-// child.on('close', (code) => {
-//   console.info(`Child process exited with code ${code}`)
-//   clearTimeout(timeout)
-// })
+process.exit(result.status)
