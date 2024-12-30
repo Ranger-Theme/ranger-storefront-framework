742e98d9a23085b00d255a347e90a3a537bec171
caesar <454451758@qq.com>
2024-12-30 21:56:26 +0800

    chore: editor cdn

--- apps/ranger-vite/components/TinymceEditor/tinymceEditor.tsx
+++ apps/ranger-vite/components/TinymceEditor/tinymceEditor.tsx
@@ -1,8 +1,25 @@
+import type { FC } from 'react'
+import type { IAllProps } from '@tinymce/tinymce-react'
 import { Editor } from '@tinymce/tinymce-react'
+
+export type ScriptType = 'domestic' | 'abroad' | 'local'
+export type ScriptMap = {
+  [key in ScriptType]: string
+}
+
+interface TinymceEditorProps extends IAllProps {
+  type: ScriptType
+}
 
 const apiKey: string = import.meta.env.REACT_APP_EDITOR_API_KEY
 
-const TinymceEditor = () => {
+const scriptSrcMap: ScriptMap = {
+  domestic: 'https://cdn.bootcdn.net/ajax/libs/tinymce/7.2.1/tinymce.min.js',
+  abroad: 'https://cdn.jsdelivr.net/npm/tinymce@7.2.1/tinymce.min.js',
+  local: '/assets/js/tinymce/tinymce.min.js'
+}
+
+const TinymceEditor: FC<TinymceEditorProps> = ({ type = 'local', ...props }) => {
   const useDarkMode: boolean = window.matchMedia('(prefers-color-scheme: dark)').matches
 
   const handleEditorChange = (content: string) => {
@@ -17,10 +34,13 @@ const TinymceEditor = () => {
   return (
     <>
       <Editor
+        {...props}
         apiKey={apiKey}
-        // tinymceScriptSrc="https://cdn.jsdelivr.net/npm/tinymce@6/tinymce.min.js"
+        tinymceScriptSrc={scriptSrcMap?.[type] ?? undefined}
         init={{
           height: 500,
+          promotion: false,
+          license_key: 'gpl',
           plugins:
             'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap code',
           editimage_cors_hosts: ['picsum.photos'],
@@ -44,6 +64,7 @@ const TinymceEditor = () => {
           skin: useDarkMode ? 'oxide-dark' : 'oxide',
           content_css: useDarkMode ? 'dark' : 'default',
           language: 'zh_CN',
+          language_url: '/assets/js/tinymce/langs/zh_CN.js',
           branding: false,
           ui_mode: 'split',
           font_size_input_default_unit: 'px',
---
+++ apps/ranger-vite/public/assets/js/tinymce/VERSION.md
@@ -0,0 +1,10 @@
+```
+.
+tinymce/
+├── skins/          # 皮肤文件
+├── themes/         # 主题文件
+├── plugins/        # 插件文件
+├── langs/          # 语言文件
+├── tinymce.min.js  # 主脚本文件
+└── ...
+```
